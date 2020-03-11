a8a70c1d208db443d8a575c435e7bddc4e5b83b5
AndrewSimons3 <simdrew07@gmail.com>
2020-03-10 20:56:15 -0500

    started

--- src/App.js
+++ src/App.js
@@ -2,25 +2,30 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isClicked: false
+    }
+  }
+
+  toggle = () => {
+    this.setState({isClicked: !this.state.isClicked});
+  }
+
+  render() {
+    console.log('***THIS IS STATE***', this.state.isClicked)
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hello World</h1>
+        <button onClick={this.toggle}>{this.state.isClicked ? 'On' : 'Off'}</button>
       </header>
     </div>
   );
+}
 }
 
 export default App;
