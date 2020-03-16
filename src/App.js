ed86d07c818458e7fc5b4cf2651c185609dfa665
AndrewSimons3 <simdrew07@gmail.com>
2020-03-15 22:09:14 -0500

    finished

--- src/App.js
+++ src/App.js
@@ -6,22 +6,54 @@ class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      isClicked: false
+      isClicked: false,
+      text: "",
+      todos: []
     }
   }
 
-  toggle = () => {
-    this.setState({isClicked: !this.state.isClicked});
+  addTodoItem = () => {
+    let todoText = this.state.text
+    let updatedTodos = [...this.state.todos, todoText]
+    this.setState({todos: updatedTodos, text: ""})
+  }
+
+  deleteTodoItem = function(index) {
+    if(this.state) {
+      let updatedTodos = this.state.todos.splice(index + 1)
+      console.log(index)
+      console.log(updatedTodos)
+      this.setState({todos: updatedTodos})
+      console.log(this)
+    }
+   
+  }
+
+  changedText = (e) => {
+    console.log(e.target.value)
+    this.setState({text: e.target.value})
   }
 
   render() {
+    console.log(this.state.todos)
     console.log('***THIS IS STATE***', this.state.isClicked)
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello World</h1>
-        <button onClick={this.toggle}>{this.state.isClicked ? 'On' : 'Off'}</button>
+        <input value={this.state.text} onChange={this.changedText} type="text"></input>
+        <button onClick={this.addTodoItem}>{this.state.isClicked ? 'On' : 'Off'}</button>
+        <div>
+        {(this.state.todos.map((todo, index) => {
+          return <div key={index}>
+            <span>
+            {todo}
+              </span>
+              <button onClick={this.deleteTodoItem.bind(this, index)}>x</button>
+          </div>
+        }))}
+      </div>
       </header>
     </div>
   );
